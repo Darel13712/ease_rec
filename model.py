a3738ae700be824ee24585078eb2cf5daba8e03a
Darel13712 <darel142857@gmail.com>
2022-04-14 17:52:02 +0300

    use multiprocessing to predict faster

--- model.py
+++ model.py
@@ -2,7 +2,7 @@ from scipy.sparse import csr_matrix
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
-from tqdm import tqdm
+from multiprocessing import Pool, cpu_count
 
 
 class EASE:
@@ -22,7 +22,11 @@ class EASE:
         implicit: if True, ratings are ignored and taken as 1, else normalized ratings are used
         """
         users, items = self._get_users_and_items(df)
-        values = np.ones(df.shape[0]) if implicit else df['rating'].to_numpy() / df['rating'].max()
+        values = (
+            np.ones(df.shape[0])
+            if implicit
+            else df['rating'].to_numpy() / df['rating'].max()
+        )
 
         X = csr_matrix((values, (users, items)))
         self.X = X
@@ -38,23 +42,32 @@ class EASE:
         self.pred = X.dot(B)
 
     def predict(self, train, users, items, k):
-        df = pd.DataFrame()
         items = self.item_enc.transform(items)
         dd = train.loc[train.user_id.isin(users)]
         dd['ci'] = self.item_enc.transform(dd.item_id)
         dd['cu'] = self.user_enc.transform(dd.user_id)
-        g = dd.groupby('user_id')
-        for user, group in tqdm(g):
-            watched = set(group['ci'])
-            candidates = [item for item in items if item not in watched]
-            u = group['cu'].iloc[0]
-            pred = np.take(self.pred[u, :], candidates)
-            res = np.argpartition(pred, -k)[-k:]
-            r = pd.DataFrame({
+        g = dd.groupby('cu')
+        with Pool(cpu_count()) as p:
+            user_preds = p.starmap(
+                self.predict_for_user,
+                [(user, group, self.pred[user, :], items, k) for user, group in g],
+            )
+        df = pd.concat(user_preds)
+        df['item_id'] = self.item_enc.inverse_transform(df['item_id'])
+        df['user_id'] = self.user_enc.inverse_transform(df['user_id'])
+        return df
+
+    @staticmethod
+    def predict_for_user(user, group, pred, items, k):
+        watched = set(group['ci'])
+        candidates = [item for item in items if item not in watched]
+        pred = np.take(pred, candidates)
+        res = np.argpartition(pred, -k)[-k:]
+        r = pd.DataFrame(
+            {
                 "user_id": [user] * len(res),
                 "item_id": np.take(candidates, res),
-                "score": np.take(pred, res)
-            }).sort_values('score', ascending=False)
-            df = df.append(r, ignore_index=True)
-        df['item_id'] = self.item_enc.inverse_transform(df['item_id'])
-        return df
+                "score": np.take(pred, res),
+            }
+        ).sort_values('score', ascending=False)
+        return r
